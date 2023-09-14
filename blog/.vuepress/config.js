6eea87f0842bf61b9026eef90fe95626b66945c6
Steve Brown <stbrown@6browns.org>
2023-09-14 13:47:59 -0400

    base instance

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,8 @@
+// .vuepress/config.js
+module.exports = {
+    title: 'BFNet Blog', // Title for the site. This will be displayed in the navbar.
+    theme: '@vuepress/theme-blog',
+    themeConfig: {
+      // Please keep looking down to see the available options.
+    }
+  }
---
+++ blog/_posts/2023-09-14-Initial-post.md
@@ -0,0 +1,14 @@
+---
+title: Hello World
+date: 2023-09-14
+tags:
+  - Vue
+  - JAMStack
+author: Steve Brown
+email: stbrown@6browns.org
+location: Fredericksburg
+---
+
+> This is the offical blog theme.
+
+...and my content.
